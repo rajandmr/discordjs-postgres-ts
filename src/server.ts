ba7b440507f1e0be1dab7a593c45f85f5ec1b6d8
dmr <prasadrajankalwar@gmail.com>
2021-06-20 13:37:42 +0545

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+import { CommandoClient } from 'discord.js-commando';
+import path from 'path';
+
+const client = new CommandoClient({
+  commandPrefix: '?',
+  owner: process.env.OWNER
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['first', 'Your first command group'],
+    ['second', 'Your second Command Group']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join(__dirname, 'features'));
+
+client.once('ready', () => {
+  // eslint-disable-next-line no-console
+  console.log('Ever Ready');
+  client.user?.setActivity('with Commando');
+});
+
+// eslint-disable-next-line no-console
+client.on('error', console.error);
+client.login(process.env.DISCORD_BOT_TOKEN);
